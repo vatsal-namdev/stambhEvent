771c31a44c12fbd3297e75a338d0b5ab87a1e337
vatsal-namdev <114605872+vatsal-namdev@users.noreply.github.com>
2023-12-17 14:51:51 +0530

    whitenoise

--- FD/settings.py
+++ FD/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "core",
-    "bootstrap5"
+    "bootstrap5",
+    "whitenoise.runserver_nostatic"
 ]
 
 MIDDLEWARE = [
