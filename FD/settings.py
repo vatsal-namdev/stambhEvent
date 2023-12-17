5f6f57308494e0941123ef324a5ff3f725133643
vatsal-namdev <114605872+vatsal-namdev@users.noreply.github.com>
2023-12-17 11:26:27 +0530

    h

--- FD/settings.py
+++ FD/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -71,7 +72,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "FD.wsgi.application"
 
-
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
